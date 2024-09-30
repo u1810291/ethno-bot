e43b87578794a3992ef4d04841cf9f3130c6299d
MahmudAbbashan <tuit.maxmudov@gmail.com>
2024-09-30 20:49:22 +0400

    .

--- git.md
+++ git.md
@@ -1,6 +1,16 @@
 # git commands
+```
 git add . //lokal
 git commit -m // faqat ozgartirilgan fayllar uchun
 git commit -am // add va commit -m birlashgani, faqatgina o'zgartirish      kirilgan fayllar uchun ishlaydi
 git push origin master
 git pull origin master
+
+
+
+```
+```javascript
+const js=12
+
+```
+TODO HALA
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express"); //load express module
+const PORT = process.env.PORT || 4040;
+
+const app = express(); 
+app.use(express.json());
+app.post("*", async (req, res) => {
+    res.send("Hello Ethno");
+});
+
+app.get("*", async (req, res) => {
+res.send("Hello, World!");
+});
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+console.log("Server listening on PORT", PORT)
+});
