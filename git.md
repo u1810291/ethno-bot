9dd4b56da3797acf92c31b61be6aace87e30e41e
MahmudAbbashan <tuit.maxmudov@gmail.com>
2024-10-11 22:28:54 +0400

    .

--- git.md
+++ git.md
@@ -1,5 +1,10 @@
 # git commands
+```
 git add . //lokal
+
 git commit -m // faqat ozgartirilgan fayllar uchun
-git commit -am // add va commit -m birlashgani, faqatgina o'zgartirish      kirilgan fayllar uchun ishlaydi
+
+git commit -am // add va commit -m birlashgani, faqatgina o'zgartirish kirilgan fayllar uchun ishlaydi
+
 git push origin main
+```
--- view/index.js
+++ view/index.js
@@ -1,37 +1,53 @@
+const {sendProduct} = require('./sendProduct');
+
 function initializeView(bot) {
   bot.onText(/\/start/, (msg) => {
     const chatId = msg?.chat?.id;
     const firstName = msg?.chat?.first_name
-
-    const reply_markup ={
-      inline_keyboard: [
-        [{ text: 'Отправить посылку', callback_data: 'button_pressed' }],
-        [{ text: 'Выкуп товара', callback_data: 'button_pressed' }],
-        [{ text: 'Наши адреса', callback_data: 'button_pressed' }]
-      ]
-    }
-    bot.sendPhoto(chatId, './assets/logistics.jpg', {
-      caption: firstName + " добро пожаловать в Ethno Logistics! \
-        Мы очень рады, что Вы выбрали нас! \
-        Как мы можем Вам помочь?:",
-      reply_markup: reply_markup,
-    },
-    {
-      filename: 'asset',
-      contentType: 'image'
-    }
-    );
+    main(bot, chatId, firstName)
+    
   });
 
   bot.on('callback_query', (callbackQuery) => {
     const message = callbackQuery.message;
     const data = callbackQuery.data;
+    const chatId = message.chat.id;
+    const firstName = message.chat.first_name;
+    console.log('sendProduct');
 
-    if (data === 'button_pressed') {
-      bot.sendMessage(message.chat.id, 'You pressed the button!');
+    if (data === 'send_product') {
+        sendProduct(bot, message.chat);
+        return;
     }
+    if (data === 'dokument') {
+        bot.sendMessage(message.chat.id, 'dokument bosildi') 
+      }
+   if (data === 'kirish') {
+        main(bot, chatId, firstName) 
+      }
   });
 
 }
+function main(bot, chatId, firstName){
+  const kirish ={
+    inline_keyboard: [
+      [{ text: 'Отправить посылку', callback_data: 'send_product' }],
+      [{ text: 'Выкуп товара', callback_data: 'buy_product' }],
+      [{ text: 'Наши адреса', callback_data: 'adress' }],
+      [{ text: 'отслеживание посылки', callback_data: 'tracking'}]
+    ]
+  }
+  bot.sendPhoto(chatId, './assets/logistics.jpg', {
+    caption: firstName + " добро пожаловать в Ethno Logistics! \
+      Мы очень рады, что Вы выбрали нас! \
+      Как мы можем Вам помочь?:",
+    reply_markup: kirish,
+  },
+  {
+    filename: 'asset',
+    contentType: 'image'
+  }
+  );
+}
 
-module.exports = { initializeView }
+module.exports = {initializeView};
