a5e71709a42bb690d291dab8755af170a714ab6d
u1810291 <otabekbutcher@gmail.com>
2025-01-05 22:37:02 +0400

    fix: fixed loations

--- src/view/locations.js
+++ src/view/locations.js
@@ -1,21 +1,19 @@
 const { bot } = require('../config/bot.config');
 
 function location(chatId){
-  const img = './assets/logistics.jpg';
-  const caption = "Наши адреса";
   const reply_markup = {
     inline_keyboard: [
-        [{ text: 'Ташкент',  url: 'https://t.me/ethnologisticsuz/1247'}],
-        [{ text: 'Дубай',  url: 'https://t.me/ethnologisticsuz/1335'}],
-        [{ text: 'Стамбул',  url: 'https://t.me/ethnologisticsuz/1253'}],
-        [{ text: 'Алматы',  url: 'https://t.me/ethnologisticsuz/1255'}],
-        [{ text: 'Бишкек',  url: 'https://t.me/ethnologisticsuz/1249'}],
-        [{ text: 'Москва',  url: 'https://t.me/ethnologisticsuz/1258'}],
-        [{ text: 'Душанбе',  url: 'https://t.me/ethnologisticsuz/1251'}],
-        [{ text: 'nazad',  url: ''}]
+      [{ text: 'Ташкент',  url: 'https://t.me/ethnologisticsuz/1247'}],
+      [{ text: 'Дубай',  url: 'https://t.me/ethnologisticsuz/1335'}],
+      [{ text: 'Стамбул',  url: 'https://t.me/ethnologisticsuz/1253'}],
+      [{ text: 'Алматы',  url: 'https://t.me/ethnologisticsuz/1255'}],
+      [{ text: 'Бишкек',  url: 'https://t.me/ethnologisticsuz/1249'}],
+      [{ text: 'Москва',  url: 'https://t.me/ethnologisticsuz/1258'}],
+      [{ text: 'Душанбе',  url: 'https://t.me/ethnologisticsuz/1251'}],
+      [{ text: 'Назад',  url: 'menu'}]
     ]
   }
-  bot.sendPhoto(chatId, img, { caption, reply_markup });
+  bot.sendMessage(chatId, "Пожалуйста выберите адрес:", { reply_markup });
 }
 
 module.exports = { location }
